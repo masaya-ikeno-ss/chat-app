c738e9c242eb6b2f253956ee1fcbbbf4054fc806
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-04-23 14:27:06 +0900

    ユーザー編集機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/UserController.java
+++ src/main/java/in/tech_camp/chat_app/controller/UserController.java
@@ -2,17 +2,21 @@ package in.tech_camp.chat_app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import in.tech_camp.chat_app.entity.UserEntity;
+import in.tech_camp.chat_app.form.EditUserForm;
 import in.tech_camp.chat_app.form.LoginForm;
 import in.tech_camp.chat_app.form.UserForm;
 import in.tech_camp.chat_app.repository.UserRepository;
 import in.tech_camp.chat_app.service.UserService;
 import lombok.AllArgsConstructor;
+
 
 
 
@@ -41,7 +45,20 @@ public class UserController {
     }
     return "users/login";
   }
-  
+
+  @GetMapping("/users/{id}/edit")
+  public String showEditPage(@PathVariable("id") Integer id, Model model) {
+    UserEntity userEntity = userRepository.findById(id);
+
+    EditUserForm userForm = new EditUserForm();
+    userForm.setId(userEntity.getId());
+    userForm.setName(userEntity.getName());
+    userForm.setEmail(userEntity.getEmail());
+
+    model.addAttribute("user", userForm);
+    return "users/edit";
+  }
+
   @PostMapping("/user")
   public String createUser(@ModelAttribute("userForm") UserForm userForm, Model model) {
 
@@ -59,4 +76,25 @@ public class UserController {
     }
     return "redirect:/";
   }
+
+  @PostMapping("/users/{id}")
+  public String updateUser(
+    @ModelAttribute("userForm") @Validated EditUserForm editUserForm,
+    @PathVariable("id") Integer id,
+    Model model
+    ) {
+      UserEntity user = userRepository.findById(id);
+      user.setName(editUserForm.getName());
+      user.setEmail(editUserForm.getEmail());
+
+      try {
+        userRepository.update(user);
+      } catch (Exception e) {
+        System.out.println("エラー" + e);
+        model.addAttribute("editUserForm", editUserForm);
+        return "users/edit";
+      }
+      return "redirect:/";
+  }
+  
 }
