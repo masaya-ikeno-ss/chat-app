2bb03e3894244bd78358a059ee0e5f9a4ab9838d
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-04-24 11:37:29 +0900

    messagesテーブルの作成

---
+++ src/main/resources/db/migration/V4__create_messages_table.sql
@@ -0,0 +1,11 @@
+create table messages(
+  id SERIAL NOT NULL,
+  content VARCHAR(512),
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  image VARCHAR(512),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE
+)
