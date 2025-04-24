4d26bfbb20c7140608559ff4f09bf6aec9370d1f
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-04-24 13:15:45 +0900

    メッセージ送信機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/MessageRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/MessageRepository.java
@@ -1,8 +1,14 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.chat_app.entity.MessageEntity;
 
 @Mapper
 public interface MessageRepository {
-  
+  @Insert("INSERT INTO messages(user_id, room_id, content) VALUES(#{user.id}, #{room.id}, #{content})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void insert(MessageEntity messageEntity);
 }
